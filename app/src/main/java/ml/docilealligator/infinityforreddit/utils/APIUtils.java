5f8c1cbed42471e04868bf85ff7df28bcfeb152b
Luca <jeremylan@hotmail.com>
2023-07-12 14:07:51 +1000

    Removed API key - and invalidate old one

--- app/src/main/java/ml/docilealligator/infinityforreddit/utils/APIUtils.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/utils/APIUtils.java
@@ -5,6 +5,7 @@ import android.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
+import ml.docilealligator.infinityforreddit.BuildConfig;
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
 
@@ -28,7 +29,7 @@ public class APIUtils {
 
     public static final String CLIENT_ID_KEY = "client_id";
     public static final String CLIENT_SECRET_KEY = "client_secret";
-    public static final String CLIENT_ID = "kNPp6nTfZ_O7s05wgz-ELQ";
+    public static final String CLIENT_ID = BuildConfig.REDDIT_API_KEY;
     public static final String IMGUR_CLIENT_ID = "Client-ID cc671794e0ab397";
     public static final String REDGIFS_CLIENT_ID = "1828d0bcc93-15ac-bde6-0005-d2ecbe8daab3";
     public static final String REDGIFS_CLIENT_SECRET = "TJBlw7jRXW65NAGgFBtgZHu97WlzRXHYybK81sZ9dLM=";
